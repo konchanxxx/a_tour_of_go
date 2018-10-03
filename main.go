6630d9515fe5e9022a41af470026ef05fb7f9310
rexitorg <rexit.org@gmail.com>
2018-10-04 02:02:12 +0900

    ponters

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("counting")
-	for i := 0; i < 10; i++ {
-		defer fmt.Println(i)
-	}
-	fmt.Println("done")
+	i, j := 42, 2701
+
+	p := &i
+	fmt.Println(*p)
+	*p = 21
+	fmt.Println(i)
+
+	p = &j
+	*p = *p / 37
+	fmt.Println(j)
 }
