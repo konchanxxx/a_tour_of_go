aaa1c71763f3d6363c9c210916f76afc66112b57
rexitorg <rexit.org@gmail.com>
2018-10-03 16:01:51 +0900

    type conversions

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"math"
+	"fmt"
+)
 
 func main() {
-	var i int
-	var f float64
-	var b bool
-	var s string
-	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+	var x, y = 3, 4
+	var f = math.Sqrt(float64(x*x + y*y))
+	var z = uint(f)
+	fmt.Println(x, y, z)
 }
