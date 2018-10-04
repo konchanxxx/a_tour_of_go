08c5122cb85f38105d99b7876fd5d28bdd0338f9
rexitorg <rexit.org@gmail.com>
2018-10-04 22:40:20 +0900

    map literals

--- main.go
+++ main.go
@@ -6,10 +6,15 @@ type Vertex struct {
 	Lat, Long float64
 }
 
+var m = map[string]Vertex{
+	"Bell Labs": Vertex{
+		40.68433, -74.39957,
+	},
+	"Google": Vertex{
+		37.42202, -122.08408,
+	},
+}
+
 func main() {
-	m := map[string]Vertex{}
-	m["Bell Labs"] = Vertex{
-		40.68433, -74.39967,
-	}
-	fmt.Println(m["Bell Labs"])
+	fmt.Println(m)
 }
