ed19a85b8b62b3a748f575c0b53909ceeb6d0cba
rexitorg <rexit.org@gmail.com>
2018-10-04 20:25:21 +0900

    exercise slices

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
-import "fmt"
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	image := make([][]uint8, dy)
+	for y := range image {
+		image[y] = make([]uint8, dx)
+	}
+
+	for y := 0; y < dy; y++ {
+		for x := 0; x < dx; x++ {
+			image[y][x] = uint8(x * y)
+		}
+	}
+	return image
+}
 
 func main() {
-	pow := make([]int, 10)
-	for i := range pow {
-		pow[i] = 1 << uint(i)
-	}
-	for _, value := range pow {
-		fmt.Printf("%d\n", value)
-		// 2進数
-		fmt.Printf("%b\n", value)
-	}
+	pic.Show(Pic)
 }
