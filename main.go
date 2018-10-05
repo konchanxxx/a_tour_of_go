7e5a7c0632fd137f762541bfc57c4e5db182055c
rexitorg <rexit.org@gmail.com>
2018-10-05 17:15:49 +0900

    interfaces are implemented implicitly

--- main.go
+++ main.go
@@ -1,40 +1,20 @@
 package main
 
-import (
-	"math"
-	"fmt"
-)
+import "fmt"
 
-type Abser interface {
-	Abs() float64
+type I interface {
+	M()
 }
 
-type MyFloat float64
-
-func (f MyFloat) Abs() float64 {
-	if f < 0 {
-		return float64(-f)
-	}
-	return float64(f)
+type T struct {
+	S string
 }
 
-type Vertex struct {
-	X, Y float64
-}
-
-func (v *Vertex) Abs() float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+func (t T) M() {
+	fmt.Println(t.S)
 }
 
 func main() {
-	var a Abser
-	f := MyFloat(-math.Sqrt2)
-	v := Vertex{3, 4}
-
-	a = f
-	a = &v
-
-	//a = v
-
-	fmt.Println(a.Abs())
+	var i T = T{"hello"}
+	i.M()
 }
