8e4619df535d4158a8987311b9e58018bf61b399
rexitorg <rexit.org@gmail.com>
2018-10-06 19:56:01 +0900

    excersize readers

--- main.go
+++ main.go
@@ -1,20 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"strings"
+	"golang.org/x/tour/reader"
 )
 
-func main() {
-	r := strings.NewReader("Hello, Reader!")
-	b := make([]byte, 8)
-	for {
-		n, err := r.Read(b)
-		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
-		fmt.Printf("b[:n] = %q\n", b[:n])
-		if err == io.EOF {
-			break
-		}
+type MyReader struct{}
+
+func (r MyReader) Read(b []byte) (int, error) {
+	for i := range b {
+		b[i] = 'A'
 	}
+	return len(b), nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
 }
