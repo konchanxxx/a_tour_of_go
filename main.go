3fba3ae9fed8ddcb24a1cd5246100e29b4811105
rexitorg <rexit.org@gmail.com>
2018-10-06 19:27:54 +0900

    exercise errors

--- main.go
+++ main.go
@@ -2,27 +2,38 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"math"
 )
 
-type MyError struct {
-	When time.Time
-	What string
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %f", e)
 }
 
-func (e *MyError) Error() string {
-	return fmt.Sprintf("at %v, %s", e.When, e.What)
-}
+const eqs = 1e-9
 
-func run() error {
-	return &MyError{
-		time.Now(),
-		"it didn't work",
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
 	}
+	z := 1.0
+	p := z
+
+	fmt.Println(eqs)
+
+	for {
+		z = z - (z*z-x)/(2*z)
+		if math.Abs(z-p) < eqs {
+			break
+		}
+		p = z
+	}
+
+	return z, nil
 }
 
 func main() {
-	if err := run(); err != nil {
-		fmt.Println(err)
-	}
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
 }
