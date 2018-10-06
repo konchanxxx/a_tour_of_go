3dbb9859282b23a2be5e8bd9026db8a75ebc79a1
rexitorg <rexit.org@gmail.com>
2018-10-06 19:43:09 +0900

    readers

--- main.go
+++ main.go
@@ -2,38 +2,19 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"io"
+	"strings"
 )
 
-type ErrNegativeSqrt float64
-
-func (e ErrNegativeSqrt) Error() string {
-	return fmt.Sprintf("cannot Sqrt negative number: %f", e)
-}
-
-const eqs = 1e-9
-
-func Sqrt(x float64) (float64, error) {
-	if x < 0 {
-		return 0, ErrNegativeSqrt(x)
-	}
-	z := 1.0
-	p := z
-
-	fmt.Println(eqs)
-
+func main() {
+	r := strings.NewReader("Hello, Reader!")
+	b := make([]byte, 8)
 	for {
-		z = z - (z*z-x)/(2*z)
-		if math.Abs(z-p) < eqs {
+		n, err := r.Read(b)
+		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q\n", b[:n])
+		if err == io.EOF {
 			break
 		}
-		p = z
 	}
-
-	return z, nil
-}
-
-func main() {
-	fmt.Println(Sqrt(2))
-	fmt.Println(Sqrt(-2))
 }
