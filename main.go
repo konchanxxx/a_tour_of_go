4beaab95ffcd247ef2fac31a0d444c8109148b01
rexitorg <rexit.org@gmail.com>
2018-10-06 21:47:51 +0900

    images

--- main.go
+++ main.go
@@ -1,42 +1,12 @@
 package main
 
 import (
-	"io"
-	"os"
-	"strings"
+	"fmt"
+	"image"
 )
 
-type rot13Reader struct {
-	r io.Reader
-}
-
-func rot13(b byte) byte {
-	if 'A' <= b && b <= 'Z' {
-		k := b - 'A'
-		return 'A' + (k-13+26)%26
-	}
-	if 'a' <= b && b <= 'z' {
-		k := b - 'a'
-		return 'a' + (k-13+26)%26
-	}
-	return b
-}
-
-func (rot *rot13Reader) Read(p []byte) (int, error) {
-	nr, ne := rot.r.Read(p)
-	if ne != nil {
-		return nr, ne
-	}
-
-	for i := 0; i < nr; i++ {
-		p[i] = rot13(p[i])
-	}
-
-	return nr, nil
-}
-
 func main() {
-	s := strings.NewReader("lbh penpxrq gur pbqr!")
-	r := rot13Reader{s}
-	io.Copy(os.Stdout, &r)
+	m := image.NewRGBA(image.Rect(0, 0, 100, 100))
+	fmt.Println(m.Bounds())
+	fmt.Println(m.At(0, 0).RGBA())
 }
