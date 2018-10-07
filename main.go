97c3d5033e08c30cd40c542dc21b5f2a36ca976e
rexitorg <rexit.org@gmail.com>
2018-10-07 22:48:02 +0900

    goroutines

--- main.go
+++ main.go
@@ -1,26 +1,18 @@
 package main
 
 import (
-	"golang.org/x/tour/pic"
-	"image"
-	"image/color"
+	"fmt"
+	"time"
 )
 
-type Image struct{}
-
-func (i Image) ColorModel() color.Model {
-	return color.RGBAModel
-}
-
-func (i Image) Bounds() image.Rectangle {
-	return image.Rect(0, 0, 256, 256)
-}
-
-func (i Image) At(x, y int) color.Color {
-	return color.RGBA{uint8(x), uint8(y), 255, 255}
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
 }
 
 func main() {
-	m := Image{}
-	pic.ShowImage(m)
+	go say("world")
+	say("hello")
 }
